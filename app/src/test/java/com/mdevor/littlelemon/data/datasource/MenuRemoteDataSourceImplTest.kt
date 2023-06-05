602554b0fb8966ac0fc484ce1c00c79cba3c4b6e
mirianfonkam <28691174+mirianfonkam@users.noreply.github.com>
2023-06-05 08:06:05 -0300

    add data source test

---
+++ app/src/test/java/com/mdevor/littlelemon/data/datasource/MenuRemoteDataSourceImplTest.kt
@@ -0,0 +1,41 @@
+package com.mdevor.littlelemon.data.datasource
+
+import com.mdevor.littlelemon.data.remote.datasource.MenuRemoteDataSourceImpl
+import com.mdevor.littlelemon.data.remote.service.MenuService
+import com.mdevor.littlelemon.testhelpers.stubs.getDomainMenuList
+import io.mockk.coEvery
+import io.mockk.mockk
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class MenuRemoteDataSourceImplTest {
+
+    private val service: MenuService = mockk()
+    private val dataSource = MenuRemoteDataSourceImpl(service)
+
+    @Test
+    fun `GIVEN service succeeds WHEN getMenu THEN a list of MenuItem`() = runBlocking {
+        // GIVEN
+        coEvery { service.getMenu() } returns getDomainMenuList()
+        val expectedResult = getDomainMenuList()
+
+        // WHEN
+        val actualResult = dataSource.getMenu()
+
+        // THEN
+        assertEquals(expectedResult, actualResult)
+    }
+
+    @Test
+    fun `GIVEN service fails WHEN getMenu THEN throw exception`() = runBlocking {
+        // GIVEN
+        coEvery { service.getMenu() } throws Exception()
+
+        // WHEN
+        val actualResult = runCatching { dataSource.getMenu() }
+
+        // THEN
+        assert(actualResult.isFailure)
+    }
+}
