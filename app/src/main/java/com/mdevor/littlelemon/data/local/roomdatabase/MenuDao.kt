fad4cf481e6bd42e396e456130b2daf499881d7e
mirianfonkam <28691174+mirianfonkam@users.noreply.github.com>
2023-07-01 21:36:35 -0400

    fix local data source impl

--- app/src/main/java/com/mdevor/littlelemon/data/local/roomdatabase/MenuDao.kt
+++ app/src/main/java/com/mdevor/littlelemon/data/local/roomdatabase/MenuDao.kt
@@ -1,15 +1,15 @@
 package com.mdevor.littlelemon.data.local.roomdatabase
 
-import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
 import com.mdevor.littlelemon.data.local.entity.MenuLocalEntity
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface MenuDao {
     @Query("SELECT * FROM menu")
-    fun getAll(): LiveData<List<MenuLocalEntity>>
+    fun getAll(): Flow<List<MenuLocalEntity>>
 
     @Insert
     fun insertAll(vararg menuItems: MenuLocalEntity)
